50a2c6b710a4438041dc0fc0f7dd268af7654e53
ghvinerias <ghvinerias@gmail.com>
2023-11-20 14:57:33 +0400

    fixed some more

--- inserts_users.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Aleksandre","Surname":"Ghvineria","email":"aleksandre@slick.ge"}' http://localhost:5000/store_data
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Dachi","Surname":"Arghutashvili","email":"Dachi@slick.ge"}' http://localhost:5000/store_data
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Endelika","Surname":"Mtsariashvili","email":"Endelika@slick.ge"}' http://localhost:5000/store_data
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Lana","Surname":"Janezashvili","email":"Lana@slick.ge"}' http://localhost:5000/store_data
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Natali","Surname":"Jeiranashvili","email":"Natali@slick.ge"}' http://localhost:5000/store_data
-curl -X POST -H "Content-Type: application/json" -d '{"group":"participants","Name":"Mikheli","Surname":"Shubitidze","email":"Mikheli@slick.ge"}' http://localhost:5000/store_data
