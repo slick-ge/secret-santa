1ffa5e0ad8eb25d93773bb6d298c8ef845b9cd88
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-11-23 03:03:13 +0400

    add redirect to dashboard

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
-export default function Home() {
+import { getAuth } from "./_lib/actions";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const auth = await getAuth();
+  if (!auth) {
+    redirect("/dashboard");
+  }
   return (
     <main className="flex flex-col gap-5">
       <Link className="px-4 py-2 border border-white text-center" href="/login">
