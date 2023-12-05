db8eb5a0646355ff697fe2c798dd495215fbb82a
Aleksandre Ghvineria <Ghvinerias@gmail.com>
2023-12-05 08:05:50 +0000

    added functionality for Testing Mode, API Version :1.8

--- API/api.py
+++ API/api.py
@@ -1,174 +1,19 @@
-from flask import Flask, request
-from pymongo import MongoClient
-from bson import ObjectId
+from flask import Flask, request, jsonify
 import json
-import os
-import re
-import random
-import smtplib
-from email.message import EmailMessage
 import logging
-
-#from dotenv import load_dotenv
-
-#load_dotenv()
+import secret_santas_functions as secret_santa
+import os
 
 logging.basicConfig(level=logging.INFO)
 
-MONGO_HOST = os.getenv('MONGODB_HOST')
-MONGO_PORT = int(os.getenv('MONGODB_PORT'))
-MONGO_DB = os.getenv('MONGODB_DB')
-MONGO_USER = os.getenv('MONGODB_USER')
-MONGO_PASS = os.getenv('MONGODB_PASS')
-
-client = MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}")
-db = client[MONGO_DB]
-
-
-#print(f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}")
-
-SENDER_EMAIL = os.getenv('SMTP_SENDER_EMAIL')
-EMAIL_SUBJECT = os.getenv('SMTP_SUBJECT')
-SMTP_SERVER = os.getenv('SMTP_URL')  # Replace with your SMTP server
-SMTP_PORT = int(os.getenv('SMTP_PORT'))  # Replace with your SMTP server's port
-SMTP_USERNAME = os.getenv('SMTP_USER')
-SMTP_PASSWORD = os.getenv('SMTP_PASW')
-
-def db_healthcheck():
-    try:
-        db_ping = db.command('ping')
-        if db_ping:
-            return "Healthy"
-    except ConnectionFailure:
-        logging.info("Could Not Connect To DB")
-        return "Unhealthy"
-
-def validate_email(email):
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return bool(re.match(pattern, email))
-
-def insert_user(collection, user_data):
-    try:
-        if not validate_email(user_data["email"]):
-            logging.info("Invalid email. Not inserting.")
-            return "Invalid email. Not inserting."
-
-        # Check for existing user with the same email
-        existing_email = collection.find_one({"email": user_data["email"]})
-        if existing_email:
-            logging.info("User with this email already exists. Not inserting.")
-            return "User with this email already exists. Not inserting."
-
-        # Check for existing user with same details
-        existing_user = collection.find_one({
-            "Name": user_data["Name"],
-            "Surname": user_data["Surname"],
-            "email": user_data["email"]
-        })
-
-        if existing_user:
-            logging.info("User with same name, surname, and email already exists. Not inserting.")
-            return "User with same name, surname, and email already exists. Not inserting."
-        user_data.pop('group')
-        result = collection.insert_one(user_data)
-        if result.inserted_id:
-            logging.info('Data stored in MongoDB successfully!')
-            update_pipeline(collection.database, collection.name)  
-            return "Data stored in MongoDB successfully!"
-        else:
-            logging.info('Failed to store data in MongoDB.')
-            return "Failed to store data in MongoDB."
-    except Exception as e:
-        logging.info(f"Error inserting user data: {e}")
-        return f"Error inserting user data: {e}"
-        
-        
-def update_pipeline(database, collection_name):
-    try:
-        pipeline_collection = database["rooms"]
-        existing_collection = pipeline_collection.find_one({"collection_name": collection_name})
-        if not existing_collection:
-            pipeline_collection.insert_one({"collection_name": collection_name})
-            logging.info(f"Pipeline updated with collection: {collection_name}")
-        else:
-            logging.info(f"Collection '{collection_name}' already exists in 'rooms'. Skipping update.")
-    except Exception as e:
-        logging.info(f"Error updating pipeline: {e}")
-
-def secret_santa(ids):
-    shuffled_ids = list(ids)
-    random.shuffle(shuffled_ids)
-    assignments = {}
-
-    for i in range(len(ids)):
-        while True:
-            recipient = random.choice(shuffled_ids)
-            if recipient != ids[i]:
-                break
-        assignments[ids[i]] = recipient
-        shuffled_ids.remove(recipient)
-
-    return assignments
-
-def print_secret_santa(collection, assignments):
-    for santa, recipient in assignments.items():
-        santa_id = ObjectId(santa)
-        recipient_id = ObjectId(recipient)
-
-        santa_details = collection.find_one({"_id": santa_id})
-        recipient_details = collection.find_one({"_id": recipient_id})
-
-        if santa_details and recipient_details:
-            logging.info(f"{santa_details['Name']} {santa_details['Surname']} ({santa_details['email']}) "
-                  f"is the Secret Santa for {recipient_details['Name']} {recipient_details['Surname']} "
-                  f"({recipient_details['email']})")
-        else:
-            logging.info(f"Participant details not found for Santa ID: {santa}, Recipient ID: {recipient}")
-
-def send_secret_santa(collection, assignments):
-    for santa, recipient in assignments.items():
-        santa_id = ObjectId(santa)
-        recipient_id = ObjectId(recipient)
-
-        santa_details = collection.find_one({"_id": santa_id})
-        recipient_details = collection.find_one({"_id": recipient_id})
-
-        if santa_details and recipient_details:
-            receiver_email = f"{santa_details['email']}"
-            email_body = f"Congratulations {santa_details['Name']} {santa_details['Surname']}, you are Secret Santa for {recipient_details['Name']} {recipient_details['Surname']}"
-            logging.info(receiver_email)
-            logging.info(email_body)
-            send_email(SENDER_EMAIL, receiver_email, EMAIL_SUBJECT, email_body, SMTP_SERVER, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD)
-        else:
-            logging.info(f"Participant details not found for Santa ID: {santa}, Recipient ID: {recipient}")
-
-def send_email(SENDER_EMAIL, receiver_email, subject, body, SMTP_SERVER, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD):
-    try:
-        msg = EmailMessage()
-        msg.set_content(body)
-        msg['Subject'] = subject
-        msg['From'] = SENDER_EMAIL
-        msg['To'] = receiver_email
-
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-#            server.connect()
-            server.starttls()
-            server.login(SMTP_USERNAME, SMTP_PASSWORD)
-            server.send_message(msg)
-#            server.close()
-        logging.info("Email sent successfully!")
-        return "Email sent successfully!"
-    except Exception as e:
-        logging.info(f"Error sending email: {e}")
-        return f"Error sending email: {e}"
-
+APP_ENV = os.getenv('APP_ENV')
 
 app = Flask(__name__)
 
 @app.route('/secret-santa/health', methods=['GET'])
 def health():
     try:
-        status = db_healthcheck()
+        status = secret_santa.db_healthcheck()
         if not status:
             return "Unhealthy", 400
         return "Healthy", 200
@@ -183,28 +28,40 @@ def store_data():
         group_name = data.get('group')  # Extract collection name from JSON
         if not group_name:
             return "Collection name not provided in the request", 400
-        group = db[group_name]
-        response = insert_user(group, data)
+        group = secret_santa.db[group_name]
+        response = secret_santa.insert_user(group, data)
         return response , 200
     except Exception as e:
         return f"Error storing user data: {e}", 500
 
 @app.route('/secret-santa/randomize_secret_santa', methods=['POST'])
 def randomize_secret_santa():
+
     try:
         data = request.get_json()
         group_name = data.get('group')  # Extract collection name from JSON
         if not group_name:
             return "Collection name not provided in the request", 400
         
-        collection = db[group_name]  # Use the collection name dynamically
+        if APP_ENV == "Test":
+            print("Testing Mode")
+            collection = secret_santa.db[group_name]  # Use the collection name dynamically
+            cursor = collection.find()
+            documents = list(cursor)
+            json_documents = json.dumps(documents, default=str, indent=1)
+            json_data = json.loads(json_documents)
+            ids = [doc['_id'] for doc in json_data]
+            assignments = secret_santa.secret_santa(ids)
+            secret_santa_json = secret_santa.print_secret_santa(collection, assignments)
+            return secret_santa_json, 200
+        collection = secret_santa.db[group_name]  # Use the collection name dynamically
         cursor = collection.find()
         documents = list(cursor)
         json_documents = json.dumps(documents, default=str, indent=1)
         json_data = json.loads(json_documents)
         ids = [doc['_id'] for doc in json_data]
-        assignments = secret_santa(ids)
-        send_secret_santa(collection, assignments)
+        assignments = secret_santa.secret_santa(ids)
+        secret_santa.send_secret_santa(collection, assignments)
         return "Secret Santa assignments randomized and emails sent!"
     except Exception as e:
         return f"Error randomizing Secret Santa: {e}", 500
