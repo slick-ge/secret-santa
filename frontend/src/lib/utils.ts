362a9838418e558ec937b89494a23eba5871b49b
Misho Shubitidze <misho@mshub.dev>
2024-09-10 18:29:15 +0400

    rm public folder

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -7,6 +7,16 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.BASE_URL) return `https://${process.env.BASE_URL}`;
   return "http://localhost:3000";
+}
+
+export function getApiUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.API_URL) return `https://${process.env.API_URL}`;
+  return "http://localhost:8080";
+}
+
+export function getApiUrlWithPath(path: string) {
+  return `${getApiUrl()}/${path}`;
 }
