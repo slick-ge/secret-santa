fa1240c721d51dc62a342f447a24cd0f103e37fa
Aleksandre Ghvineria <Ghvinerias@gmail.com>
2024-11-26 13:27:43 +0000

    cleanup

--- frontend/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="min-h-[calc(100dvh-7rem)]">
-      <div className="container mx-auto py-10">Dashboard</div>
-    </div>
-  );
-}
--- frontend/src/components/icons.tsx
+++
@@ -1,7 +0,0 @@
-import { RectangleEllipsisIcon } from "lucide-react";
-
-type IconProps = React.HTMLAttributes<SVGElement>;
-
-export const Icons = {
-  logo: (props: IconProps) => <RectangleEllipsisIcon {...props} />,
-};
